f5e3aee67763036c0e76b91ee8cc58dd9709b567
Matt Taylor <matt.r.taylor@gmail.com>
2017-12-05 12:52:08 -0800

    Update ver-update.sh
    Added additional comments and removed the see-saw commands to start the associated cron-host GCP instance.

--- ver-update.sh
+++ ver-update.sh
@@ -2,8 +2,10 @@
 #!/bin/bash
 
 #Global Varibles
+	#Query and parse mojang JSON of versions and parse out most current
 	MCJSON=$(curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json |awk -F\" ' {print $10} ')
 #		printf "${MCJSON}\n"
+	#Version currently running on the server, if any
 	CURRENTVER=$(ls -al  /home/minecraft |grep -v ".old" |grep minecraft |awk -F\. ' {print $2"."$3"."$4}')
 #		printf "${CURRENTVER}\n"
 	
@@ -12,15 +14,16 @@
 
 #Version Check Function
 vercheck () {
-	if [ -z "$CURRENTVER" ]; then 
+	if [ -z "$CURRENTVER" ]; then #Server does not have a current version
 		printf "No current version file found, downloading latest version\n"
 		getlatest
 	else
-		if [ $CURRENTVER != $MCJSON ]; then
+		if [ $CURRENTVER != $MCJSON ]; then #If the current version doesn't match the Mojang current version
 	
 			printf "New Server Version is Available: ${MCJSON}\n"
 	       		printf "Moving existing .jar file to .old\n"
-		
+			
+			#Arvhive old versions for potential roll back			
 			FILENAME="/home/minecraft/minecraft_server."$CURRENTVER".jar"
 #				printf "${FILENAME}\n"
 			OLDFILENAME="/home/minecraft/oldversions/minecraft_server."$CURRENTVER".jar.old"
@@ -30,8 +33,11 @@ vercheck () {
 
 			printf "\n\n"
 			printf "Downloading latst version\n"
+			
+			#Run the function to download the new latest server version
 			getlatest
 		else
+			#Server is running the Mojang current version and no action needed
 			printf "Server Version is Current\n"
 		fi
 	fi
@@ -45,18 +51,14 @@ getlatest () {
 }
 
 launchlatest () {
-
+	#Function to launch the server, create and save the running screen session.
+	
 	LAUNCHVER="minecraft_server."$MCJSON".jar"
 	printf "$LAUNCHVER\n"
 	LAUNCHSTRING="screen -d -m -S mcs java -Xms1g -Xmx7G -d64 -jar "$LAUNCHVER" nogui"
 	printf "$LAUNCHSTRING\n"
 	sudo $LAUNCHSTRING
 	
-}
-
-cronhandoff () {
-
-	gcloud compute instances stop taylor-mc-cron-host --zone=us-central1-c
 }
 
 #-------------------
